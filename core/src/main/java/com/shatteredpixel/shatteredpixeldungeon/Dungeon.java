2a740e885766dabf568e393bf257a50e8bd1ac7b
JDSA Ling <CutelinXXX@126.com>
2025-08-10 22:19:02 +0800

    Boss楼层追加30层，以避免渲染空楼层

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/Dungeon.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/Dungeon.java
@@ -352,7 +352,7 @@ public class Dungeon {
 	}
 	
 	public static boolean bossLevel( int depth ) {
-		return depth == 5 || depth == 10 || depth == 15 || depth == 20 || depth == 25;
+		return depth == 5 || depth == 10 || depth == 15 || depth == 20 || depth == 25 || depth == 30;
 	}
 	
 	public static void switchLevel( final Level level, int pos ) {
