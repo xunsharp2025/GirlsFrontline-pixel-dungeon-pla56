a943d3159724914770f9757abdf970b91b4b85c2
JDSA Ling <CutelinXXX@126.com>
2025-08-13 18:00:33 +0800

    军方小队追加 深入敌腹 挑战词条难度

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/Typhootin.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/Typhootin.java
@@ -2,33 +2,49 @@ package com.shatteredpixel.shatteredpixeldungeon.actors.mobs;
 
 import com.shatteredpixel.shatteredpixeldungeon.Assets;
 import com.shatteredpixel.shatteredpixeldungeon.Badges;
+import com.shatteredpixel.shatteredpixeldungeon.Challenges;
 import com.shatteredpixel.shatteredpixeldungeon.Dungeon;
 import com.shatteredpixel.shatteredpixeldungeon.actors.Actor;
 import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
 import com.shatteredpixel.shatteredpixeldungeon.actors.blobs.Freezing;
+import com.shatteredpixel.shatteredpixeldungeon.actors.blobs.ToxicGas;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Barrier;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Buff;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.FlavourBuff;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.FrostImbue;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Light;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.LockedFloor;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Paralysis;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Terror;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.ToxicImbue;
 import com.shatteredpixel.shatteredpixeldungeon.effects.CellEmitter;
 import com.shatteredpixel.shatteredpixeldungeon.effects.Speck;
 import com.shatteredpixel.shatteredpixeldungeon.effects.particles.ElmoParticle;
+import com.shatteredpixel.shatteredpixeldungeon.effects.particles.PoisonParticle;
 import com.shatteredpixel.shatteredpixeldungeon.items.Ankh;
 import com.shatteredpixel.shatteredpixeldungeon.items.artifacts.LloydsBeacon;
 import com.shatteredpixel.shatteredpixeldungeon.items.keys.SkeletonKey;
 import com.shatteredpixel.shatteredpixeldungeon.items.scrolls.ScrollOfPsionicBlast;
+import com.shatteredpixel.shatteredpixeldungeon.items.scrolls.ScrollOfTeleportation;
 import com.shatteredpixel.shatteredpixeldungeon.items.weapon.enchantments.Grim;
 import com.shatteredpixel.shatteredpixeldungeon.levels.Level;
 import com.shatteredpixel.shatteredpixeldungeon.levels.Terrain;
 import com.shatteredpixel.shatteredpixeldungeon.messages.Messages;
 import com.shatteredpixel.shatteredpixeldungeon.scenes.GameScene;
+import com.shatteredpixel.shatteredpixeldungeon.sprites.CharSprite;
+import com.shatteredpixel.shatteredpixeldungeon.sprites.CyclopsSprite;
+import com.shatteredpixel.shatteredpixeldungeon.sprites.HydraSprite;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.TyphootinSprite;
 import com.shatteredpixel.shatteredpixeldungeon.ui.BossHealthBar;
 import com.shatteredpixel.shatteredpixeldungeon.utils.GLog;
 import com.watabou.noosa.Camera;
 import com.watabou.noosa.audio.Sample;
 import com.watabou.utils.Bundle;
+import com.watabou.utils.PathFinder;
 import com.watabou.utils.Random;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Typhootin extends Mob {
 
@@ -44,6 +60,95 @@ public class Typhootin extends Mob {
 
         properties.add(Property.BOSS);
     }
+
+    public static int[] ArmyPos = new int[]{
+            815,551,566,302
+    };
+    @Override
+    public boolean act() {
+        boolean bleeding = (HP*2 <= HT);
+        if(bleeding && Dungeon.isChallenged(Challenges.STRONGER_BOSSES) && buff(Cooldown.class) == null){
+            Mob army =Army();
+            army.state = army.HUNTING;
+            army.maxLvl = -1;
+            if(army instanceof Hydra){
+                army.HP = army.HT = 140;
+            }
+            GameScene.add(army);
+            int randomPosIndex = Random.Int(ArmyPos.length);
+            int randomPos = ArmyPos[randomPosIndex];
+            ScrollOfTeleportation.appear(army,randomPos);
+
+            if(Random.Int(4)==0){
+                Cyclops cyclops = new Cyclops();
+                cyclops.state = cyclops.HUNTING;
+                cyclops.maxLvl = -1;
+                GameScene.add(cyclops);
+                int cyclopsrandomPosIndex = Random.Int(ArmyPos.length);
+                int cyclopsrandomPos = ArmyPos[cyclopsrandomPosIndex];
+                ScrollOfTeleportation.appear(cyclops,cyclopsrandomPos);
+            }
+
+            sprite.centerEmitter().start( Speck.factory( Speck.SCREAM ), 0.3f, 3 );
+            Sample.INSTANCE.play( Assets.Sounds.CHALLENGE );
+            yell(Messages.get(this,"army"));
+            Buff.affect(this, Cooldown.class, 20f);
+            Buff.affect(this, Barrier.class).setShield(20);
+        }
+
+        return super.act();
+    }
+
+    public static class Cooldown extends FlavourBuff { }
+
+    private Mob Army() {
+        List<Class<? extends Mob>> mobTypes = new ArrayList<>();
+        mobTypes.add(Hydra.class);
+        mobTypes.add(CyclopsArmy.class);
+
+        Random.shuffle(mobTypes);
+        Class<? extends Mob> selectedMobType = mobTypes.get(0);
+        Mob mob = null;
+        try {
+            mob = selectedMobType.getDeclaredConstructor().newInstance();
+        } catch (Exception ignored) {}
+        return mob;
+    }
+
+    public static class CyclopsArmy extends Cyclops{
+        {
+            HP = HT = 100;
+            EXP = 17;
+            defenseSkill = 30;
+            baseSpeed = 1f;
+            maxLvl = -1;
+            properties.add(Property.ARMO);
+        }
+
+        @Override
+        public void damage( int dmg, Object src ) {
+            super.damage( dmg, src );
+
+            if (isAlive() && !enraged && HP < HT / 4) {
+                enraged = true;
+                spend( TICK );
+                if (Dungeon.level.heroFOV[pos]) {
+                    sprite.showStatus( CharSprite.NEGATIVE, Messages.get(this, "enraged") );
+                }
+            }
+        }
+
+        private boolean enraged = false;
+
+        @Override
+        public int damageRoll() {
+            return enraged ?
+                    Random.NormalIntRange( 30, 65 ) :
+                    Random.NormalIntRange( 25, 50 );
+        }
+    }
+
+
 
     @Override
     public int damageRoll() {
@@ -124,6 +229,12 @@ public class Typhootin extends Mob {
             beacon.upgrade();
         }
 
+        for (Mob mob : Dungeon.level.mobs.toArray(new Mob[0])){
+            if (mob instanceof CyclopsArmy || mob instanceof Hydra) {
+                mob.die( cause );
+            }
+        }
+
         yell( Messages.get(this, "defeated") );
     }
 
@@ -140,13 +251,19 @@ public class Typhootin extends Mob {
     }
 
     {
-        immunities.add( Freezing.class );
-        immunities.add( Terror.class );
+        immunities.add(Freezing.class);
+        immunities.add(Terror.class);
+        if (Dungeon.isChallenged(Challenges.STRONGER_BOSSES)) {
+             immunities.add(ToxicGas.class);
+        }
     }
 
     @Override
-    public void restoreFromBundle(Bundle bundle) {
-        super.restoreFromBundle(bundle);
-        BossHealthBar.assignBoss(this);
+    public void restoreFromBundle( Bundle bundle ) {
+        super.restoreFromBundle( bundle );
+        if (state != SLEEPING) BossHealthBar.assignBoss(this);
+        if ((HP*2 <= HT)) BossHealthBar.bleed(true);
     }
+
+
 }
