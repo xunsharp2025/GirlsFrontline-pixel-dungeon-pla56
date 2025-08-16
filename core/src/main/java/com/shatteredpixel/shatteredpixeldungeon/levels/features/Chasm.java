13de485259c3d4b0634e6af033a35b4684cb8da8
Cat-Zs <2995051320@qq.com>
2025-08-16 10:08:56 +0800

    调整了坠落音效的音量大小为原来的2/3

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/features/Chasm.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/features/Chasm.java
@@ -84,7 +84,7 @@ public class Chasm implements Hero.Doom {
 		
 		jumpConfirmed = false;
 				
-		Sample.INSTANCE.play( Assets.Sounds.AIC2 );
+		Sample.INSTANCE.play(Assets.Sounds.AIC2,0.67f);
 
 		TimekeepersHourglass.timeFreeze timeFreeze = Dungeon.hero.buff(TimekeepersHourglass.timeFreeze.class);
 		if (timeFreeze != null) timeFreeze.disarmPressedTraps();
