c1f333d8af5a2afc8d40c94c363a2980fb834b02
Cat-Zs <2995051320@qq.com>
2025-09-09 19:23:03 +0800

    无实质修改

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/hero/abilities/type561/StationNearby.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/hero/abilities/type561/StationNearby.java
@@ -1,26 +1,27 @@
 package com.shatteredpixel.shatteredpixeldungeon.actors.hero.abilities.type561;
 
-/*
-public class NaturesPower extends ArmorAbility {
+import com.shatteredpixel.shatteredpixeldungeon.effects.particles.LeafParticle;
+import com.shatteredpixel.shatteredpixeldungeon.items.armor.ClassArmor;
+import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Hero;
+import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Talent;
+import com.shatteredpixel.shatteredpixeldungeon.actors.hero.abilities.ArmorAbility;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Invisibility;
+import com.shatteredpixel.shatteredpixeldungeon.ui.HeroIcon;
 
+public class StationNearby extends ArmorAbility {
 	{
 		baseChargeUse = 65f;
 	}
 
 	@Override
 	protected void activate(ClassArmor armor, Hero hero, Integer target) {
-
-		Buff.prolong(hero, naturesPowerTracker.class, naturesPowerTracker.DURATION);
-		hero.buff(naturesPowerTracker.class).extensionsLeft = 2;
 		hero.sprite.operate(hero.pos);
-		Sample.INSTANCE.play(Assets.Sounds.CHARGEUP);
 		hero.sprite.emitter().burst(LeafParticle.GENERAL, 10);
 
 		armor.charge -= chargeUse(hero);
 		armor.updateQuickslot();
 		Invisibility.dispel();
-		hero.spendAndNext(Actor.TICK);
-
+		hero.spendAndNext(Hero.TICK);
 	}
 
 	@Override
@@ -32,40 +33,4 @@ public class NaturesPower extends ArmorAbility {
 	public Talent[] talents() {
 		return new Talent[]{Talent.GROWING_POWER, Talent.NATURES_WRATH, Talent.WILD_MOMENTUM, Talent.HEROIC_ENERGY};
 	}
-
-	public static class naturesPowerTracker extends FlavourBuff{
-
-		public static final float DURATION = 8f;
-
-		public int extensionsLeft = 2;
-
-		public void extend( int turns ){
-			if (extensionsLeft > 0 && turns > 0) {
-				spend(turns);
-				extensionsLeft--;
-			}
-		}
-
-		@Override
-		public int icon() {
-			return BuffIndicator.SHADOWS;
-		}
-
-		@Override
-		public float iconFadePercent() {
-			return Math.max(0, (DURATION - visualcooldown()) / DURATION);
-		}
-
-		@Override
-		public String toString() {
-			return Messages.get(this, "name");
-		}
-
-		@Override
-		public String desc() {
-			return Messages.get(this, "desc", dispTurns(visualcooldown()));
-		}
-
-	}
 }
-*/
